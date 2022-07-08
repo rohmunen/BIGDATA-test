153d3fd051581a3286772d77bed690ca4861012e
Daniil Salyn <salyndaniil@gmail.com>
2022-07-08 15:37:31 +0300

    refactor

--- src/index.tsx
+++ src/index.tsx
@@ -1,52 +1,20 @@
-import { Loader, Pagination } from '@mantine/core'
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
+import React from 'react';
 import ReactDOM from 'react-dom/client'
 import {
   BrowserRouter,
   Routes,
   Route,
 } from "react-router-dom";
-import { Movie } from '../types'
-import { MainLayout } from './components/Layout'
-import { MoviesList } from './components/MoviesList'
+import { Home } from './pages/home';
 import { NotFound } from './components/NotFound';
 
+
 const root = ReactDOM.createRoot(document.getElementById('root')!)
-
-const App = () => {
-  const [ total, setTotal ] = useState(0)
-  const [ loading, setLoading ] = useState(false)
-  const [ currentPage, setCurrentPage ] = useState(1)
-  const [ movies, setMovies ] = useState<Array<Movie>>([])
-
-  useEffect(() => {
-    axios.get(`https://yts.mx/api/v2/list_movies.json?page=${currentPage}&limit=10`).then(res => {
-      setTotal(Math.floor(res.data.data.movie_count / 5) + 2)
-    })
-  }, [])
-
-  useEffect(() => {
-    setLoading(true)
-    axios.get(`https://yts.mx/api/v2/list_movies.json?page=${currentPage}&limit=5`).then(res => {
-      setMovies(res.data.data.movies)
-      setLoading(false)
-    }
-    )
-  }, [ currentPage ])
-
-  return (
-    <MainLayout>
-      <MoviesList movies={ movies } loading={ loading } />
-      <Pagination page={ currentPage } onChange={ setCurrentPage } total={ total } />
-    </MainLayout>
-  )
-}
 
 root.render(
   <BrowserRouter>
     <Routes>
-      <Route path='/' element={ <App /> } />
+      <Route path='/' element={ <Home /> } />
       <Route
         path="*"
         element={ <NotFound /> }
---
+++ src/pages/home/index.tsx
@@ -0,0 +1,37 @@
+import { Movie } from '../../../types'
+import { MainLayout } from '../../components/Layout'
+import { MoviesList } from '../../components/MoviesList'
+import { NotFound } from '../../components/NotFound';
+import { Loader, Pagination } from '@mantine/core'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+
+
+export const Home = () => {
+  const [ total, setTotal ] = useState(0)
+  const [ loading, setLoading ] = useState(false)
+  const [ currentPage, setCurrentPage ] = useState(1)
+  const [ movies, setMovies ] = useState<Array<Movie>>([])
+
+  useEffect(() => {
+    axios.get(`https://yts.mx/api/v2/list_movies.json?page=${currentPage}&limit=10`).then(res => {
+      setTotal(Math.floor(res.data.data.movie_count / 5) + 2)
+    })
+  }, [])
+
+  useEffect(() => {
+    setLoading(true)
+    axios.get(`https://yts.mx/api/v2/list_movies.json?page=${currentPage}&limit=5`).then(res => {
+      setMovies(res.data.data.movies)
+      setLoading(false)
+    }
+    )
+  }, [ currentPage ])
+
+  return (
+    <MainLayout>
+      <MoviesList movies={ movies } loading={ loading } />
+      <Pagination page={ currentPage } onChange={ setCurrentPage } total={ total } />
+    </MainLayout>
+  )
+}
