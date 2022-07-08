e55fe01c64b3278ee2505d5e10bfd0824373e0cf
Daniil Salyn <salyndaniil@gmail.com>
2022-07-08 16:19:25 +0300

    style inputs

--- src/components/Comments/Inputs/index.tsx
+++ src/components/Comments/Inputs/index.tsx
@@ -20,7 +20,7 @@ export const Inputs = (props: Props) => {
         placeholder="Ваш комментарий"
         onChange={ (e) => { setForm({ ...form, message: e.target.value }) } }
       />
-      <Button onClick={ () => { setComments(oldComments => [ ...oldComments, form ]) } } >
+      <Button className={styles.Input__button} onClick={ () => { setComments(oldComments => [ ...oldComments, form ]) } } >
         Оставить комментарий
       </Button>
     </div>
